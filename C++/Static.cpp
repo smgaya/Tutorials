5f216e0670114487ea5d5228a1a8c5185f8c4a2d
Thomas <thomas@reaney.net>
2017-01-07 17:38:54 +0000

    Initial C++ basic tutorials added

---
+++ C++/Static.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Function declaration
+void func(void);
+
+static int count = 10; /* Global variable */
+
+int main()
+{
+    while(count--)
+    {
+        func();
+    }
+
+    return 0;
+}
+
+// Function definition
+void func(void)
+{
+    static int i = 5; // Local static variable
+    i++;
+    std::cout << "i is " << i ;
+    std::cout << " and count is " << count << std::endl;
+}
